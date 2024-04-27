45626d4275d309322a7055442f82ca6eb6fa0fd0
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-04-27 17:14:05 +0600

    Login Registration completed

--- src/PublicRotes.jsx
+++ src/PublicRotes.jsx
@@ -4,8 +4,8 @@ import Tamplate from "./Tamplate.jsx";
 // import Home from "./Home/Home.jsx";
 // import UpdateProfile from "./UpdateProfile/updateProfile.jsx";
 // import CartEstate from "./CartEstate/CartEstate.jsx";
-// import Login from "./Auth/Login.jsx";
-// import Register from "./Auth/Register.jsx";
+import Login from "./Auth/Login.jsx";
+import Register from "./Auth/Register.jsx";
 // import PrivateRoutes from "./PrivateRoutes/MainPrivateRoutes.jsx";
 // import AuthPrivateRoute from "./PrivateRoutes/AuthPrivateRoute.jsx";
 // import ViewDetails from "./ViewDetails/ViewDetails.jsx";
@@ -16,60 +16,60 @@ const PublicRotes = () => {
       path: "/",
       element: <Tamplate></Tamplate>,
       errorElement: <NotFoundPage></NotFoundPage>,
-      // children: [
-      //   {
-      //     path: "/",
-      //     loader: () =>
-      //       fetch(
-      //         `https://millatsakib.github.io/img-src/assignment%209/Data/assignment9.json`
-      //       ),
-      //     element: <Home></Home>,
-      //   },
-      //   {
-      //     path: "/update_profile",
-      //     element: (
-      //       <PrivateRoutes>
-      //         <UpdateProfile></UpdateProfile>
-      //       </PrivateRoutes>
-      //     ),
-      //   },
-      //   {
-      //     path: "/cart_estate",
-      //     element: (
-      //       <PrivateRoutes>
-      //         <CartEstate></CartEstate>
-      //       </PrivateRoutes>
-      //     ),
-      //   },
-      //   {
-      //     path: "/login",
-      //     element: (
-      //       <AuthPrivateRoute>
-      //         <Login></Login>
-      //       </AuthPrivateRoute>
-      //     ),
-      //   },
-      //   {
-      //     path: "/register",
-      //     element: (
-      //       <AuthPrivateRoute>
-      //         <Register></Register>
-      //       </AuthPrivateRoute>
-      //     ),
-      //   },
-      //   {
-      //     path: "/property/:propertyID",
-      //     element: (
-      //       <PrivateRoutes>
-      //         <ViewDetails></ViewDetails>
-      //       </PrivateRoutes>
-      //     ),
-      //     loader: ({ params }) =>
-      //       fetch(
-      //         `https://millatsakib.github.io/img-src/assignment%209/Data/00${params.propertyID}.json`
-      //       ),
-      //   },
-      // ],
+      children: [
+        //   {
+        //     path: "/",
+        //     loader: () =>
+        //       fetch(
+        //         `https://millatsakib.github.io/img-src/assignment%209/Data/assignment9.json`
+        //       ),
+        //     element: <Home></Home>,
+        //   },
+        //   {
+        //     path: "/update_profile",
+        //     element: (
+        //       <PrivateRoutes>
+        //         <UpdateProfile></UpdateProfile>
+        //       </PrivateRoutes>
+        //     ),
+        //   },
+        //   {
+        //     path: "/cart_estate",
+        //     element: (
+        //       <PrivateRoutes>
+        //         <CartEstate></CartEstate>
+        //       </PrivateRoutes>
+        //     ),
+        //   },
+        {
+          path: "/login",
+          element: (
+            // <AuthPrivateRoute>
+            <Login></Login>
+            // </AuthPrivateRoute>
+          ),
+        },
+        {
+          path: "/register",
+          element: (
+            // <AuthPrivateRoute>
+            <Register></Register>
+            // </AuthPrivateRoute>
+          ),
+        },
+        //   {
+        //     path: "/property/:propertyID",
+        //     element: (
+        //       <PrivateRoutes>
+        //         <ViewDetails></ViewDetails>
+        //       </PrivateRoutes>
+        //     ),
+        //     loader: ({ params }) =>
+        //       fetch(
+        //         `https://millatsakib.github.io/img-src/assignment%209/Data/00${params.propertyID}.json`
+        //       ),
+        //   },
+      ],
     },
   ]);
   return routes;
