3c541789b9881d00644e485f3eb8af18b84c8d65
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-04-30 01:25:43 +0600

    slider updated

--- src/Home/SwipeBanner.jsx
+++ src/Home/SwipeBanner.jsx
@@ -24,34 +24,61 @@ function SwipeBanner() {
         className="mySwiper w-full select-none"
       >
         <SwiperSlide>
-          <img
-            src="https://millatsakib.github.io/img-src/Assignment10/banner/1.jpg"
-            alt=""
-          />
+          <div className="relative flex items-center justify-center">
+            <img
+              src="https://millatsakib.github.io/img-src/Assignment10/banner/1.jpg"
+              alt=""
+            />
+            <div className="text-white absolute text-xl md:text-3xl lg:text-5xl font-bold w-ful md:max-w-[80vw] lg:max-w[65vw] text-center">
+              The world is a book, and those who do not travel read only one
+              page.
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://millatsakib.github.io/img-src/Assignment10/banner/2.jpg"
-            alt=""
-          />
+          <div className="relative flex items-center justify-center">
+            <img
+              src="https://millatsakib.github.io/img-src/Assignment10/banner/2.jpg"
+              alt=""
+            />
+            <div className="text-white absolute text-xl md:text-3xl lg:text-5xl font-bold w-ful md:max-w-[80vw] lg:max-w[65vw] text-center">
+              To travel is to take a journey into yourself.
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://millatsakib.github.io/img-src/Assignment10/banner/3.jpg"
-            alt=""
-          />
+          <div className="relative flex items-center justify-center">
+            <img
+              src="https://millatsakib.github.io/img-src/Assignment10/banner/3.jpg"
+              alt=""
+            />
+            <div className="text-white absolute text-xl md:text-3xl lg:text-5xl font-bold w-ful md:max-w-[80vw] lg:max-w[65vw] text-center">
+              Traveling – it leaves you speechless, then turns you into a
+              storyteller.
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://millatsakib.github.io/img-src/Assignment10/banner/4.jpg"
-            alt=""
-          />
+          <div className="relative flex items-center justify-center">
+            <img
+              src="https://millatsakib.github.io/img-src/Assignment10/banner/4.jpg"
+              alt=""
+            />
+            <div className="text-white absolute text-xl md:text-3xl lg:text-5xl font-bold w-ful md:max-w-[80vw] lg:max-w[65vw] text-center">
+              Travel is the only thing you buy that makes you richer.
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img
-            src="https://millatsakib.github.io/img-src/Assignment10/banner/5.jpg"
-            alt=""
-          />
+          <div className="relative flex items-center justify-center">
+            <img
+              src="https://millatsakib.github.io/img-src/Assignment10/banner/5.jpg"
+              alt=""
+            />
+            <div className="text-white absolute text-xl md:text-3xl lg:text-5xl font-bold w-ful md:max-w-[80vw] lg:max-w[65vw] text-center">
+              We travel not to escape life, but for life not to escape us.
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
