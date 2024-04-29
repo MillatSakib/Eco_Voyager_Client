ac30e3eb924338e35d5a4dc3244d99c1cfaabcef
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-04-29 23:20:47 +0600

    Typewriter implemented

--- src/Home/OurSpeciality.jsx
+++ src/Home/OurSpeciality.jsx
@@ -1,33 +1,47 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 const OurSpeciality = () => {
+  const [text] = useTypewriter({
+    words: ["outword bound", "CULRURE CURIOUS", "Relax"],
+    loop: true,
+  });
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 grid-cols-1">
-      <div className="relative mx-4 flex items-center justify-center">
-        <img
-          src="https://millatsakib.github.io/img-src/Assignment10/extra/lion.jpg"
-          className="rounded-lg"
-        ></img>
-        <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
-          {" "}
-          OUTWARD BOUND
-        </span>
+    <div>
+      <div className="my-6 text-center text-xl lg:text-2xl text-orange-400">
+        Enjoy
+        <span> {text}</span>
+        <Cursor cursorStyle="<"></Cursor>
       </div>
-      <div className="relative mx-4 flex items-center justify-center">
-        <img
-          src="https://millatsakib.github.io/img-src/Assignment10/extra/cottage.jpg"
-          className="rounded-lg"
-        ></img>
-        <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
-          CULRURE CURIOUS
-        </span>
-      </div>
-      <div className="relative mx-4 flex items-center justify-center">
-        <img
-          src="https://millatsakib.github.io/img-src/Assignment10/extra/building.jpg"
-          className="rounded-lg"
-        ></img>
-        <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
-          RELAX, RELAX RELAX
-        </span>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 grid-cols-1">
+        <div className="relative mx-4 flex items-center justify-center">
+          <img
+            src="https://millatsakib.github.io/img-src/Assignment10/extra/lion.jpg"
+            className="rounded-lg"
+          ></img>
+          <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
+            {" "}
+            OUTWARD BOUND
+          </span>
+        </div>
+        <div className="relative mx-4 flex items-center justify-center">
+          <img
+            src="https://millatsakib.github.io/img-src/Assignment10/extra/cottage.jpg"
+            className="rounded-lg"
+          ></img>
+          <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
+            CULRURE CURIOUS
+          </span>
+        </div>
+        <div className="relative mx-4 flex items-center justify-center">
+          <img
+            src="https://millatsakib.github.io/img-src/Assignment10/extra/building.jpg"
+            className="rounded-lg"
+          ></img>
+          <span className="absolute text-white font-bold text-3xl md:text-2xl lg:text-4xl">
+            RELAX, RELAX RELAX
+          </span>
+        </div>
       </div>
     </div>
   );
